adcc523daf3e7c5800da672e431eaa6d6297174c
Jonatan Moreno Herrera <jonatanmh203@gmail.com>
2024-04-08 13:52:15 -0500

    Implemetation of wireflame, nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        this is a nav component
+     <nav>
+      <div className="logo">
+          <img src="/Logo.svg" alt="Logo" />
+      </div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Menu</a></li>
+        <li><a href="/Reservations">Reservations</a></li>
+        <li><a href="/OrderOnline">Order Online</a></li>
+        <li><a href="/Login">Login</a></li>
+      </ul>
     </nav>
   )
 }
