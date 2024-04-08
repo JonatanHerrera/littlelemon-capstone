b32d2058e83493e6ccc1bebdf929b78aea8ad5c1
Jonatan Moreno Herrera <jonatanmh203@gmail.com>
2024-04-08 13:16:05 -0500

    Move the folder components to src folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
