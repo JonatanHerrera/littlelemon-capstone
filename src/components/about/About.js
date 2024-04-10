c68b1cb63321a5a0c3bce594433660ce72915c5b
Jonatan Moreno Herrera <jonatanmh203@gmail.com>
2024-04-10 15:55:21 -0500

    Final General Styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,14 +4,14 @@ import './About.css'
 const About = () => {
   return (
     <div className='about-container'>
-      <div>
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+      <div className='aboutText-container'>
+        <h1 className='titleName'>Little Lemon</h1>
+        <h2 className='subTitleName'>Chicago</h2>
+        <p  className='p-n'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       </div>
-      <div>
-        <img className='aboutSectionImg' src='/restaurant chef B.jpg'/>
-        <img className='aboutSectionImg' src='/Mario and Adrian A.jpg'/>
+      <div className='aboutImages-container'>
+        <img id='about1' className='aboutSectionImg' src='/restaurant chef B.jpg'/>
+        <img id='about2' className='aboutSectionImg' src='/Mario and Adrian A.jpg'/>
       </div>
 
     </div>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,12 +4,12 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-        <div>
+        <div className='footerLogo'>
         <img src="/Logo.svg" alt="Logo2" className='Logo2'/>
         </div>
-        <div>
+        <div className='footerDiv'>
           <h2>Navigation</h2>
-          <ul>
+          <ul className='aboutList'>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/services">Menu</a></li>
@@ -18,17 +18,17 @@ const Footer = () => {
             <li><a href="/Login">Login</a></li>
           </ul>
         </div>
-        <div>
+        <div className='footerDiv'>
             <h2>Contact</h2>
-            <ul>
+            <ul className='aboutList'>
               <li>Address</li>
               <li>Phone number</li>
               <li>Email</li>
             </ul>
         </div>
-        <div>
+        <div className='footerDiv'>
           <h2>Social Media</h2>
-          <ul>
+          <ul className='aboutList'>
             <li>Facebook</li>
             <li>Instagram</li>
             <li>Whatsapp</li>
